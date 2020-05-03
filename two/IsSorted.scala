4b0f1a40264b75b49d37d266a35718ada58c5adf
nhyne <nhyne@nhyne.dev>
2020-05-03 14:04:44 -0400

    moving dirs, adding bazel

---
+++ two/IsSorted.scala
@@ -0,0 +1,29 @@
+package dev.nhyne.fp.two
+
+object IsSorted {
+    def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+        @annotation.tailrec
+        def go(n: Int) : Boolean = {
+            val next = n + 1
+            if (n == as.length - 1) {
+                true
+            }
+            else if (!ordered(as[n], as[next])) {
+                false
+            }
+            else {
+                go(n+1)
+            }
+        }
+        go(0)
+    }
+
+    def order(a: Int, b: Int): Boolean = {
+        a <= b
+    }
+
+    def main(args: Array[String]): Unit = {
+        isSorted(Seq(1, 2, 3), ordered)
+    }
+}
+
