0309c4030cc08586643762863dc03cb1a323b68c
nhyne <nhyne@nhyne.dev>
2020-07-15 17:39:38 -0400

    adding example of currying

--- two/Currying.scala
+++ two/Currying.scala
@@ -6,4 +6,11 @@ object Currying {
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
     (a: A, b: B) => f(a)(b)
   }
+
+    def main(args: Array[String]): Unit = {
+        def add2(a: Int, b: Int) = a + b
+        val addTwo = curry(add2)
+        val addThree = addTwo(3)
+        println(addThree(4))
+    }
 }
