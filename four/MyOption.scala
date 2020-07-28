2d45fb80275636e30cacfabe9cc264fc4686483b
nhyne <nhyne@nhyne.dev>
2020-07-28 18:22:06 -0400

    formatting, map2 with for

--- four/MyOption.scala
+++ four/MyOption.scala
@@ -1,46 +1,54 @@
 sealed trait MyOption[+A] {
-  def map[B](f: A => B): MyOption[B] = {
-    this match {
-      case MySome(x) => MySome(f(x))
-      case MyNone => MyNone
+    def map[B](f: A => B): MyOption[B] = {
+        this match {
+            case MySome(x) => MySome(f(x))
+            case MyNone => MyNone
+        }
     }
-  }
 
 
-  def flatMap[B](f: A => MyOption[B]): MyOption[B] = {
-      map(f) getOrElse MyNone
-  }
-  def getOrElse[B >: A](default: => B): B = {
-    this match {
-      case MySome(x) => x
-      case MyNone => default
+    def flatMap[B](f: A => MyOption[B]): MyOption[B] = {
+        map(f) getOrElse MyNone
     }
-  }
 
-  def orElse[B >:A] (ob: => MyOption[B]): MyOption[B] = {
-      this map (MySome(_)) getOrElse ob
-  }
-
-  def filter(f: A => Boolean): MyOption[A] = {
-    this match {
-      case MySome(x) if f(x) => MySome(x)
-      case MyNone => MyNone
+    def getOrElse[B >: A](default: => B): B = {
+        this match {
+            case MySome(x) => x
+            case MyNone => default
+        }
     }
-  }
 
-  def map2[A, B, C](a: MyOption[A], b: MyOption[B])(f: (A, B) => C): MyOption[C] = {
-    a.flatMap(aa => b.map(bb => f(aa, bb)))
-  }
+    def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = {
+        this map (MySome(_)) getOrElse ob
+    }
+
+    def filter(f: A => Boolean): MyOption[A] = {
+        this match {
+            case MySome(x) if f(x) => MySome(x)
+            case MyNone => MyNone
+        }
+    }
+
+    def map2[A, B, C](a: MyOption[A], b: MyOption[B])(f: (A, B) => C): MyOption[C] = {
+        a.flatMap(aa => b.map(bb => f(aa, bb)))
+    }
+
+    def forMap2[A, B, C](a: MyOption[A], b: MyOption[B])(f: (A, B) => C): MyOption[C] = {
+        for {
+            aa <- a
+            bb <- b
+        } yield f(aa, bb)
+    }
 
     def map3[A, B, C, D](a: MyOption[A], b: MyOption[B], c: MyOption[C])(f: (A, B, C) => D): MyOption[D] = {
         a.flatMap(aa => b.flatMap(bb => c.map(cc => f(aa, bb, cc))))
     }
 
-//    def something(a: Int, b: Int) = a * b + 3
-//
-//    def lift2[A, B, C](b: MyOption[B])(f: (A, B) => C): (MyOption[A], MyOption[B]) => MyOption[C] = {
-//        _ flatMap(aa => b.map( bb => f(aa, bb)))
-//    }
+    //    def something(a: Int, b: Int) = a * b + 3
+    //
+    //    def lift2[A, B, C](b: MyOption[B])(f: (A, B) => C): (MyOption[A], MyOption[B]) => MyOption[C] = {
+    //        _ flatMap(aa => b.map( bb => f(aa, bb)))
+    //    }
 
 
     def sequence[A](a: List[MyOption[A]]): MyOption[List[A]] = {
@@ -50,21 +58,27 @@ sealed trait MyOption[+A] {
         }
     }
 
+    def sequenceT[A](a: List[MyOption[A]]): MyOption[List[A]] = {
+        traverse(a)(x => x)
+    }
+
     def traverse[A, B](a: List[A])(f: A => MyOption[B]): MyOption[List[B]] = {
         a match {
             case Nil => MySome(Nil)
-//            case h :: t => f(h).flatMap(hh => traverse(t)(f) map (hh :: _))
-            case h :: t => map2( f(h), traverse(t)(f))(_ :: _)
+            //            case h :: t => f(h).flatMap(hh => traverse(t)(f) map (hh :: _))
+            case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
         }
     }
 
 }
 
 case class MySome[+A](get: A) extends MyOption[A]
+
 object MyNone extends MyOption[Nothing] {
     def main(args: Array[String]): Unit = {
 
         val someList = List("1", "2", "3", "a")
+
         def toIntMyOption(s: String) = {
             try {
                 MySome(s.toInt)
@@ -72,6 +86,7 @@ object MyNone extends MyOption[Nothing] {
                 case e: Exception => MyNone
             }
         }
+
         println(s"traversed list: ${traverse(someList)(toIntMyOption)}")
 
         val otherList = List("1", "2", "3")
