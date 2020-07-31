5d93439c08fea74f9772046bac3692608bfa451a
nhyne <nhyne@nhyne.dev>
2020-07-31 16:49:18 -0400

    beginning of chapter 5

---
+++ five/MyStream.scala
@@ -0,0 +1,75 @@
+sealed trait MyStream[+A] {
+    def toList: List[A] = {
+        this match {
+            case Empty => Nil
+            case Cons(h, t) => (h() :: t().toList)
+        }
+    }
+
+    def take(n: Int): MyStream[A] = {
+        this match {
+            case Empty => Empty
+            case Cons(h, t) => {
+                if (n <= 1) Cons(h, () => Empty)
+                else Cons(h, () => t().take(n - 1))
+            }
+        }
+    }
+
+    def drop(n: Int): MyStream[A] = {
+        this match {
+            case Empty => Empty
+            case Cons(h, t) => {
+                if (n <= 0) Cons(h, t)
+                else t().drop(n - 1)
+            }
+        }
+    }
+
+    def takeWhile(p: A => Boolean): MyStream[A] = {
+        this match {
+            case Empty => Empty
+            case Cons(h, t) => {
+                if (p(h())) Cons(h, () => t().takeWhile(p))
+                else Empty
+            }
+        }
+    }
+
+    def foldRight[B](z: => B)(f: (A, => B) => B): B =
+        this match {
+            case Cons(h, t) => f(h(), t().foldRight(z)(f))
+            case _ => z
+        }
+}
+
+case object Empty extends MyStream[Nothing]
+
+case class Cons[+A](h: () => A, t: () => MyStream[A]) extends MyStream[A]
+
+object MyStream {
+    def cons[A](hd: => A, t1: => MyStream[A]): MyStream[A] = {
+        lazy val head = hd
+        lazy val tail = t1
+
+        Cons(() => head, () => tail)
+    }
+
+    def empty[A]: MyStream[A] = Empty
+
+    def apply[A](as: A*): MyStream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+    def main(args: Array[String]): Unit = {
+        println("cool")
+
+        val testStream = MyStream("a", "b", "c")
+        val testList = testStream.toList
+        println(s"list: $testList")
+
+        println(s"taken: ${testStream.take(2).toList}")
+        println(s"dropped: ${testStream.drop(2).toList}")
+
+        def isAOrB(s: String) = s == "a" || s == "b"
+        println(s"takeWhile: ${testStream.takeWhile(isAOrB).toList}")
+    }
+}
