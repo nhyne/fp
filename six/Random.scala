1c8780de98304735c22ca74e4c4f337fccc9a867
nhyne <nhyne@nhyne.dev>
2020-08-02 12:04:02 -0400

    more rearranging

--- six/Random.scala
+++ six/Random.scala
@@ -16,47 +16,48 @@ object RNG {
             (n, nextRNG)
         }
 
-        def notNegative(rng: RNG): (Int, RNG) = {
-            val (num, newRNG) = rng.nextInt
-            if (num < 0) {
-                if (num == Int.MinValue) (Int.MaxValue, newRNG)
-                else (num * -1, newRNG)
-            } else (num, newRNG)
+    }
 
-        }
+    def notNegative(rng: RNG): (Int, RNG) = {
+        val (num, newRNG) = rng.nextInt
+        if (num < 0) {
+            if (num == Int.MinValue) (Int.MaxValue, newRNG)
+            else (num * -1, newRNG)
+        } else (num, newRNG)
 
-        def double(rng: RNG): (Double, RNG) = {
-            val (num, newRNG) = notNegative(rng)
-            (num.toDouble / Int.MaxValue, newRNG)
-        }
+    }
 
-        def intDouble(rng: RNG): ((Int, Double), RNG) = {
-            val (doub, rng2) = double(rng)
-            val (i, rng3) = rng2.nextInt
-            ((i, doub), rng3)
-        }
+    def double(rng: RNG): (Double, RNG) = {
+        val (num, newRNG) = notNegative(rng)
+        (num.toDouble / Int.MaxValue, newRNG)
+    }
 
-        def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-            val (doub, rng2) = double(rng)
-            val (i, rng3) = rng2.nextInt
-            ((doub, i), rng3)
-        }
+    def intDouble(rng: RNG): ((Int, Double), RNG) = {
+        val (doub, rng2) = double(rng)
+        val (i, rng3) = rng2.nextInt
+        ((i, doub), rng3)
+    }
 
-        def tripDouble(rng: RNG): ((Double, Double, Double), RNG) = {
-            val (doub1, rng2) = double(rng)
-            val (doub2, rng3) = double(rng2)
-            val (doub3, rng4) = double(rng3)
-            ((doub1, doub2, doub3), rng4)
+    def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+        val (doub, rng2) = double(rng)
+        val (i, rng3) = rng2.nextInt
+        ((doub, i), rng3)
+    }
 
-        }
+    def tripDouble(rng: RNG): ((Double, Double, Double), RNG) = {
+        val (doub1, rng2) = double(rng)
+        val (doub2, rng3) = double(rng2)
+        val (doub3, rng4) = double(rng3)
+        ((doub1, doub2, doub3), rng4)
 
-        def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-            if (count <= 0) (List.empty[Int], rng)
-            else {
-                val (i, rng2) = rng.nextInt
-                val (nextCall, rng3) = ints(count - 1)(rng2)
-                (i :: nextCall, rng3)
-            }
+    }
+
+    def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+        if (count <= 0) (List.empty[Int], rng)
+        else {
+            val (i, rng2) = rng.nextInt
+            val (nextCall, rng3) = ints(count - 1)(rng2)
+            (i :: nextCall, rng3)
         }
     }
 
@@ -64,22 +65,22 @@ object RNG {
 
         val rng = Random(12345)
 
-        val (nonNeg1, rng1) = rng.notNegative(rng)
-        val (nonNeg2, rng2) = rng.notNegative(rng1)
-        val (nonNeg3, rng3) = rng.notNegative(rng2)
-        val (nonNeg4, rng4) = rng.notNegative(rng3)
+        val (nonNeg1, rng1) = RNG.notNegative(rng)
+        val (nonNeg2, rng2) = RNG.notNegative(rng1)
+        val (nonNeg3, rng3) = RNG.notNegative(rng2)
+        val (nonNeg4, rng4) = RNG.notNegative(rng3)
 
         println(s"non negatives: $nonNeg1, $nonNeg2, $nonNeg3, $nonNeg4")
 
-        val (doub1, rng5) = rng.double(rng4)
-        val (doub2, rng6) = rng.double(rng5)
-        val (doub3, rng7) = rng.double(rng6)
-        val (doub4, rng8) = rng.double(rng7)
+        val (doub1, rng5) = RNG.double(rng4)
+        val (doub2, rng6) = RNG.double(rng5)
+        val (doub3, rng7) = RNG.double(rng6)
+        val (doub4, rng8) = RNG.double(rng7)
 
         println(s"doubles: $doub1, $doub2, $doub3, $doub4")
 
 
-        println(s"ints(4): ${rng.ints(4)(rng)}")
-        println(s"ints(10): ${rng.ints(10)(rng8)}")
+        println(s"ints(4): ${RNG.ints(4)(rng)}")
+        println(s"ints(10): ${RNG.ints(10)(rng8)}")
     }
 }
