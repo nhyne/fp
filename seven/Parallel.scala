6ff69b243d8f578d3561f5d0f7ad58e0d6fb81aa
nhyne <nhyne@nhyne.dev>
2020-08-05 21:26:41 -0400

    signature for sums

--- seven/Parallel.scala
+++ seven/Parallel.scala
@@ -1,15 +1,21 @@
 package seven
 
-trait Parallel {
+object Parallel {
     case class Par[A](v: A)
 
-    def unit[A](a: => A): Par[A]
+    def unit[A](a: => A): Par[A] = ???
 
-    def get[A](a: => Par[A]): A
-}
+    def get[A](a: => Par[A]): A = ???
 
-object Parallel {
+    def sums(ints: IndexedSeq[Int]): Int =
+        if (ints.size <= 1) ints.headOption.getOrElse(0)
+        else {
+            val (l, r) = ints.splitAt(ints.length / 2)
+            val sumL: Par[Int] = Parallel.unit(sums(l))
+            val sumR: Par[Int] = Parallel.unit(sums(r))
 
+            Parallel.get(sumL) + Parallel.get(sumR)
+        }
 
 }
 
