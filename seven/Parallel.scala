afaf4f17a22c874ae88ff77e244c3e08ed4740a8
nhyne <nhyne@nhyne.dev>
2020-08-24 18:08:06 -0400

    sequence

--- seven/Parallel.scala
+++ seven/Parallel.scala
@@ -1,20 +1,19 @@
-package seven
-
 object Parallel {
     case class Par[A](v: A)
 
     def unit[A](a: A): Par[A] = ???
     def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-    def run[A](a: => Par[A]): A = {
-        ???
-    }
+    def run[A](a: => Par[A]): A = ???
 
     def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => Par[C]) : Par[C] = ???
 
     def fork[A](a: => Par[A]): Par[A] = ???
 
-    def sums(ints: IndexedSeq[Int]): Int =
+    def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+        ps.foldRight[Par[List[A]]](unit(List()))((h,t) => map2(h,t)((a, b) => unit(a :: b)))
+
+    def sums(ints: IndexedSeq[Int]): Int = {
         if (ints.size <= 1) ints.headOption.getOrElse(0)
         else {
             val (l, r) = ints.splitAt(ints.length / 2)
@@ -23,10 +22,10 @@ object Parallel {
 
             Parallel.run(sumL) + Parallel.run(sumR)
         }
+    }
 
 
     def main(args: Array[String]): Unit = {
         println("we're in parallel main!")
     }
 }
-
