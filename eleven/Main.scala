4cd1b739a98c25601d7a69b65cf14c68c8158233
nhyne <nhyne@nhyne.dev>
2020-12-15 20:39:49 -0500

    some chapter 11 work

---
+++ eleven/Main.scala
@@ -0,0 +1,57 @@
+package eleven
+import six.RNG
+import six.RNG.Rand
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    flatMap(fa)(a => unit(f(a)))
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
+  def sequence[A](lma: List[F[A]]): F[List[A]] =
+    lma.foldRight(unit(List[A]()))((a, acc) => map2(a, acc)(_ :: _))
+  def sequenceLeft[A](lfa: List[F[A]]): F[List[A]] =
+    lfa.foldLeft(unit(List[A]()))((acc, a) => map2(a, acc)(_ :: _))
+  def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+    sequence(la.map(f))
+  def theirTraverse[A, B](ls: List[A])(f: A => F[B]): F[List[B]] =
+    ls.foldLeft(unit(List[B]()))((acc, a) => map2(f(a), acc)(_ :: _))
+}
+
+object Monad {
+  val genMonad = new Monad[Rand] {
+    override def unit[A](a: => A): Rand[A] = RNG.unit(a)
+
+    override def flatMap[A, B](fa: Rand[A])(f: A => Rand[B]): Rand[B] =
+      RNG.flatMap(fa)(f)
+  }
+
+  val optionMonad = new Monad[Option] {
+    def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] =
+      fa.flatMap(f)
+
+    def unit[A](a: => A): Option[A] = Some(a)
+  }
+
+  val listMonad = new Monad[List] {
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] =
+      fa.flatMap(f)
+
+    override def unit[A](a: => A): List[A] = List(a)
+  }
+}
+
+object Main {
+  val listFunctor = new Functor[List] {
+    def map[A, B](as: List[A])(f: A => B): List[B] = as.map(f)
+  }
+
+  def main(args: Array[String]): Unit =
+    println("something")
+}
